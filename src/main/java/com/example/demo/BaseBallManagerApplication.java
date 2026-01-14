fb505d6fb75860a049af7a213d104f96c474797f
kajiharuhyyy <kajiharu0921@gmail.com>
2026-01-14 23:24:04 +0900

    要件定義

---
+++ src/main/java/com/example/demo/BaseBallManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaseBallManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaseBallManagerApplication.class, args);
+	}
+
+}
