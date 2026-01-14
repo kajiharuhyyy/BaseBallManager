0541f4f707316d7488fb4d77b555f01679d99e72
kajiharuhyyy <kajiharu0921@gmail.com>
2026-01-15 00:54:46 +0900

    テーブル作成

---
+++ src/main/java/com/example/demo/domain/Player.java
@@ -0,0 +1,40 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "players")
+@Getter
+@NoArgsConstructor
+public class Player {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(nullable = false, length = 50)
+	private String name;
+	
+	@Column(nullable = false, length = 20)
+	private String position;
+	
+	@Column(nullable = false)
+	private Integer uniformNumber;
+	
+	@Builder
+	public Player(String name, String position, int uniformNumber) {
+		this.name = name;
+		this.position = position;
+		this.uniformNumber = uniformNumber;
+	}
+
+}
