18bb5a259569fea91b80e682d6c347ecc1ed3020
kajiharuhyyy <kajiharu0921@gmail.com>
2026-01-18 01:00:35 +0900

    PlayerForm作成

--- src/main/java/com/example/demo/web/form/PlayerForm.java
+++ src/main/java/com/example/demo/web/form/PlayerForm.java
@@ -1,5 +1,8 @@
 package com.example.demo.web.form;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import lombok.Data;
 
 @Data
@@ -7,9 +10,12 @@ public class PlayerForm {
 
 	private Long id;
 	
+	@NotBlank(message = "選手を入れてください")
 	private String name;
 	
+	@NotNull(message = "ポジションを選択してください")
 	private String position;
 	
+	@NotBlank(message = "背番号を入れてください")
 	private Integer uniformNumber;
 }
