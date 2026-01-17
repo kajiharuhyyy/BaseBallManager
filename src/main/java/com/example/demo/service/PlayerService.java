386a2c31adaa15a090650d1853bdef62b726e6e4
kajiharuhyyy <kajiharu0921@gmail.com>
2026-01-17 23:41:04 +0900

    PlayerService,作成

---
+++ src/main/java/com/example/demo/service/PlayerService.java
@@ -0,0 +1,76 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.domain.Player;
+import com.example.demo.repo.PlayerRepository;
+import com.example.demo.web.form.PlayerForm;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class PlayerService {
+
+	private final PlayerRepository playerRepository; 
+	
+	public List<Player> search(String name, String position, Integer uniformNumber) {
+		
+		return playerRepository.findAll().stream()
+				.filter(p -> name == null || name.isBlank() || p.getName().contains(name.trim()))
+				.filter(p -> position == null || position.isBlank() || p.getPosition().contains(position.trim()))
+				.filter(p -> uniformNumber == null || p.getUniformNumber().equals(uniformNumber))
+				.toList();
+		
+//		List<Player> players = playerRepository.findAll();
+		
+//		// 何も指定がなければ全件返す
+//		if ((name != null || name.isBlank()) 
+//				&& (position == null || position.isBlank()) 
+//				&& uniformNumber == null) {
+//			return playerRepository.findAll();
+//		}
+//		
+//		// 背番号が入ってるならそれで検索する
+//		if (uniformNumber != null) {
+//			return playerRepository.findByUniformNumber(uniformNumber);
+//		}
+//		
+//		// 名前検索
+//		if (name!= null && !name.isBlank()) {
+//			return playerRepository.findByName(name);
+//		}
+//		
+//		// ポジション検索
+//		if (position != null && !position.isBlank()) {
+//			return playerRepository.findByPosition(position);
+//		}
+		
+//		return playerRepository.findAll();
+	}
+	
+	/**
+	 * 登録処理
+	 * @param form
+	 * @return　保存
+	 */
+	@Transactional
+	public Player create(PlayerForm form) {
+		Player player = Player.builder()
+				.name(form.getName())
+				.position(form.getPosition())
+				.uniformNumber(form.getUniformNumber())
+				.build();
+		
+		return playerRepository.save(player);
+	}
+	
+	public Player getById(Long id) {
+		return playerRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid plaer ID: " + id));
+	}
+}
---
+++ src/main/java/com/example/demo/web/form/PlayerForm.java
@@ -0,0 +1,15 @@
+package com.example.demo.web.form;
+
+import lombok.Data;
+
+@Data
+public class PlayerForm {
+
+	private Long id;
+	
+	private String name;
+	
+	private String position;
+	
+	private Integer uniformNumber;
+}
