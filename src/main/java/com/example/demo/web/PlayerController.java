a5522b776b1d8ed3380ea6f4242df620f0aea290
kajiharuhyyy <kajiharu0921@gmail.com>
2026-01-21 16:31:02 +0900

    PlayerController作成

--- src/main/java/com/example/demo/web/PlayerController.java
+++ src/main/java/com/example/demo/web/PlayerController.java
@@ -1,5 +1,44 @@
 package com.example.demo.web;
 
-public class PlayerController {
+import java.util.List;
 
-}
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.demo.domain.Player;
+import com.example.demo.service.PlayerService;
+import com.example.demo.web.form.PlayerForm;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+@RequiredArgsConstructor
+public class PlayerController {
+	
+	private final PlayerService playerService;
+	
+	@GetMapping("/players")
+	public String listTeamMember(
+			@RequestParam(required = false) String name,
+			@RequestParam(required = false) String position,
+			@RequestParam(required = false) Integer uniformNumber,
+			Model model) {
+		
+		List<Player> players = playerService.search(name, position, uniformNumber);
+		
+		model.addAttribute("players", players);
+		return "players/list";
+	}
+	
+	@PostMapping("/plsyers/new")
+	public String newTeamMember(Model model) {
+		PlayerForm form = new PlayerForm();
+		
+		model.addAttribute("playerForm", form);
+		return "players/new";
+	}
+
+}	
--- src/main/java/com/example/demo/web/form/PlayerForm.java
+++ src/main/java/com/example/demo/web/form/PlayerForm.java
@@ -2,6 +2,7 @@ package com.example.demo.web.form;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -11,9 +12,11 @@ public class PlayerForm {
 	private Long id;
 	
 	@NotBlank(message = "選手を入れてください")
+	@Size(max = 50)
 	private String name;
 	
 	@NotNull(message = "ポジションを選択してください")
+	@Size(max = 20)
 	private String position;
 	
 	@NotBlank(message = "背番号を入れてください")
