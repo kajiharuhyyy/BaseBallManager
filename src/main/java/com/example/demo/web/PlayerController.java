ccf6e2977748c992c0a5461eb73c02b9edd802b8
kajiharuhyyy <kajiharu0921@gmail.com>
2026-01-21 16:59:19 +0900

    PlayerController作成

--- src/main/java/com/example/demo/web/PlayerController.java
+++ src/main/java/com/example/demo/web/PlayerController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -59,5 +60,41 @@ public class PlayerController {
 		playerService.create(form);
 		return "redirect:/players";
 	}
+	
+	@GetMapping("/players/{id}/edit")
+    public String editTeamMember(@PathVariable Long id,
+            Model model) {
+        Player player = playerService.getById(id);
+
+        PlayerForm form = new PlayerForm();
+        form.setId(player.getId());
+        form.setName(player.getName());
+        form.setPosition(player.getPosition()); 
+        form.setUniformNumber(player.getUniformNumber());
+
+        model.addAttribute("playerForm", form);
+        return "players/edit";
+    }
+
+    @PostMapping("/players/{id}/edit")
+    public String updateTeamMember(@PathVariable Long id, 
+            @Valid @ModelAttribute PlayerForm form, 
+            BindingResult bindingResult, 
+            Model model) {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("playerForm", form);
+            return "players/edit";
+        }
+
+        playerService.update(id, form);
+        return "redirect:/players"; 
+    }
+
+    @PostMapping("/players/{id}/delete")
+    public String deleteTeamMember(@PathVariable Long id) {
+        playerService.delete(id);
+        return "redirect:/players"; 
+    }
 
 }	
